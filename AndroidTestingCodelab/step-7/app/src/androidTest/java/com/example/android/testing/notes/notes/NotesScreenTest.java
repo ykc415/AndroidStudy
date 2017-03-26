a86cf6739a44f88de88a1af0adc5c44e1ba63a4f
ykc <pica415@gmail.com>
2017-03-26 20:53:07 +0900

    Java 프로젝트를 Kotlin 으로 바꾸는 예제

--- AndroidTestingCodelab/step-7/app/src/androidTest/java/com/example/android/testing/notes/notes/NotesScreenTest.java
+++ AndroidTestingCodelab/step-7/app/src/androidTest/java/com/example/android/testing/notes/notes/NotesScreenTest.java
@@ -16,6 +16,13 @@
 
 package com.example.android.testing.notes.notes;
 
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+
 import com.example.android.testing.notes.R;
 
 import org.hamcrest.Description;
@@ -25,20 +32,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.view.View;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.RecyclerViewActions.scrollTo;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -46,7 +44,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.google.common.base.Preconditions.checkArgument;
 import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.fail;
 
 /**
  * Tests for the notes screen, the main screen which contains a grid of all notes.
@@ -97,38 +94,38 @@ public class NotesScreenTest {
 
     @Test
     public void clickAddNoteButton_opensAddNoteUi() throws Exception {
-        fail("Implement step 7");
-//        // Click on the add note button
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Check if the add note screen is displayed
-//        onView(withId(R.id.add_note_title)).check(matches(isDisplayed()));
+
+        // Click on the add note button
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        // Check if the add note screen is displayed
+        onView(withId(R.id.add_note_title)).check(matches(isDisplayed()));
     }
 
     @Test
     public void addNoteToNotesList() throws Exception {
-        fail("Implement step 7");
-//        String newNoteTitle = "Espresso";
-//        String newNoteDescription = "UI testing for Android";
-//
-//        // Click on the add note button
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Add note title and description
-//        // Type new note title
-//        onView(withId(R.id.add_note_title)).perform(typeText(newNoteTitle), closeSoftKeyboard());
-//        onView(withId(R.id.add_note_description)).perform(typeText(newNoteDescription),
-//                closeSoftKeyboard()); // Type new note description and close the keyboard
-//
-//        // Save the note
-//        onView(withId(R.id.fab_add_notes)).perform(click());
-//
-//        // Scroll notes list to added note, by finding its description
-//        onView(withId(R.id.notes_list)).perform(
-//                scrollTo(hasDescendant(withText(newNoteDescription))));
-//
-//        // Verify note is displayed on screen
-//        onView(withItemText(newNoteDescription)).check(matches(isDisplayed()));
+
+        String newNoteTitle = "Espresso";
+        String newNoteDescription = "UI testing for Android";
+
+        // Click on the add note button
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        // Add note title and description
+        // Type new note title
+        onView(withId(R.id.add_note_title)).perform(typeText(newNoteTitle), closeSoftKeyboard());
+        onView(withId(R.id.add_note_description)).perform(typeText(newNoteDescription),
+                closeSoftKeyboard()); // Type new note description and close the keyboard
+
+        // Save the note
+        onView(withId(R.id.fab_add_notes)).perform(click());
+
+        // Scroll notes list to added note, by finding its description
+        onView(withId(R.id.notes_list)).perform(
+                scrollTo(hasDescendant(withText(newNoteDescription))));
+
+        // Verify note is displayed on screen
+        onView(withItemText(newNoteDescription)).check(matches(isDisplayed()));
     }
 
 }
